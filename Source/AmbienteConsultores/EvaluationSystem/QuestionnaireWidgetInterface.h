bd40a3ddd290ce99fb37d4d03f5479ce224980b8
Christian Lopierre <54386076+lopierrep@users.noreply.github.com>
2023-11-22 19:13:32 -0500

    Improve Gesture detection

--- Source/AmbienteConsultores/EvaluationSystem/QuestionnaireWidgetInterface.h
+++ Source/AmbienteConsultores/EvaluationSystem/QuestionnaireWidgetInterface.h
@@ -1,20 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
-#include "AnswerWidgetInterface.generated.h"
+#include "QuestionnaireWidgetInterface.generated.h"
 
 DECLARE_DYNAMIC_DELEGATE_OneParam(FOnAnswerReleasd, int32, AnswerNumber);
+DECLARE_DYNAMIC_DELEGATE(FOnQuestionnaireFinished);
 
 UINTERFACE()
-class UAnswerWidgetInterface : public UInterface
+class UQuestionnaireWidgetInterface : public UInterface
 {
 	GENERATED_BODY()
 };
 
-class AMBIENTECONSULTORES_API IAnswerWidgetInterface
+class AMBIENTECONSULTORES_API IQuestionnaireWidgetInterface
 {
 	GENERATED_BODY()
 
@@ -27,6 +28,12 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void IAnswerFeedback(const bool Correct, const int32 AnswerNumber);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void IShowResults(const int32 Score, const int32 TotalQuestions);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void IBindToOnQuestionnaireFinished(const FOnQuestionnaireFinished& Event);
 
 
 };
